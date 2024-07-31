c87dcb9871added38df75440d45c26422afefffe
26385 <2638502607@qq.com>
2024-08-01 00:18:18 +0800

    2024/8/1 构建项目

---
+++ src/main/java/com/example/myvedio/MyvedioApplication.java
@@ -0,0 +1,13 @@
+package com.example.myvedio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyvedioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyvedioApplication.class, args);
+    }
+
+}
